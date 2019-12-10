7628dbcac4f50d0494bd74bbc3f10158c41e21cc
Michael Shen <michael.pocheng.shen@gmail.com>
2019-12-10 18:02:49 -0500

    Worked on styling for sessionform

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -24,38 +24,63 @@ class SessionForm extends React.Component {
     }
 
     render() {
-        const form = this.props.formType === 'signup' ? (
+        const altForm = this.props.formType === 'signup' ? (
             <div>
-                <h1>Sign Up</h1>
-                <Link to='/login'>Login instead</Link>
+                <h2>By creating an account, you are agreeing to our Terms of Service and Privacy Policy.</h2>
+                <h3>Already have an account?</h3>
+                <Link to='/login'>Sign in</Link>
             </div>
         ) : (
             <div>
-                <h1>Log In</h1>
-                <Link to='/signup'>Sign up instead</Link>
+                <label>
+                    <input type="checkbox"/>
+                    <span class="checkmark">Remember me for 30 days</span>
+                </label>
+                <h3>Don't have an account?</h3>
+                <Link to='/signup'>Create account</Link>
             </div>
         );
         return (
-            <div>
-                {this.props.errors.join(" | ")}
-                {form}
-                <form>
-                    <label>Email:
+            <div class="sessionform-background">
+                <div class="sessionform">
+                    <img className="sessionform-icon" src="assets/penguin-icon.png" alt="Penguin by sandra from the Noun Project"></img>
+                    <h1>Evernote</h1>
+                    <h2>Remember everything important.</h2>
+
+                    <h3>Demo user</h3>
+
+                    <div class="line">  
+                        <div>or</div>  
+                    </div>
+                    
+                    <form className="sessionform-form">
+                        
                         <input
                             type="text"
+                            name="textfield"
                             value={this.state.email}
                             onChange={this.update("email")}
+                            placeholder="Email"
+
                         />
-                    </label>
-                    <label>Password:
+                    
+                    
                         <input
                             type="password"
+                            name="textfield"
                             value={this.state.password}
                             onChange={this.update("password")}
+                            placeholder="Password"
+
                         />
-                    </label>
-                    <button onClick={this.handleSubmit}>{this.props.formType}</button>
-                </form>
+                        
+                        {/* <button onClick={this.handleSubmit}>{this.props.formType}</button> */}
+                        <button onClick={this.handleSubmit}>Continue</button>
+                    </form>
+
+                    {altForm}
+                    {this.props.errors.join(" | ")}
+                </div>
             </div>
         )
     }
